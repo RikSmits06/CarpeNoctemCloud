ddbbb731eef8d4cc08a8a04bcc1d8cf8bf54c127
Rik Smits <h.j.smits@student.utwente.nl>
2025-11-11 09:51:27 +0100

    Multi page support

---
+++ adminPanel/database_page.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+
+import util.dbLayer as dbLayer
+
+# The database info section.
+st.title("Database")
+
+# The db size table.
+db = dbLayer.DbLayer()
+db_tables = db.get_all_tables()
+db_table_frame = pd.DataFrame(
+    db_tables,
+    columns=["Name", "Size", "Rows"]
+)
+st.table(db_table_frame)
--- adminPanel/server.py
+++ adminPanel/server.py
@@ -1,19 +1,6 @@
-import pandas as pd
 import streamlit as st
 
-import dbLayer
-
-db = dbLayer.DbLayer()
-
-st.title("Carpe Noctem Cloud Admin Panel")
-
-# The database info section.
-st.header("Database")
-
-# The db size table.
-db_tables = db.get_all_tables()
-db_table_frame = pd.DataFrame(
-    db_tables,
-    columns=["Name", "Size", "Rows"]
-)
-st.table(db_table_frame)
+pg = st.navigation(
+    {"Database": [
+        st.Page("database_page.py", title="Database")]})
+pg.run()
