8cd5d026976a2dcb5c56239a2ad0fbc826e1e5fc
Thore <thorekoopmans@gmail.com>
2025-11-21 17:29:58 +0100

    Removed downloads from search page and added them to inspect page

--- src/main/java/org/carpenoctemcloud/controllers/FileInspectController.java
+++ src/main/java/org/carpenoctemcloud/controllers/FileInspectController.java
@@ -40,10 +40,11 @@ public class FileInspectController {
     @GetMapping({"/{id}", "/{id}/"})
     public String fileInspectPage(Model model, @PathVariable int id) {
         Optional<RemoteFile> file = fileService.getRemoteFileByID(id);
-        String server = file.get().downloadURL().replaceFirst("^file:/*", "").split("/")[0];
+        //String server = file.get().downloadURL().replaceFirst("^file:/*", "").split("/")[0];
+
 
         model.addAttribute("resultFile", file.get());
-        model.addAttribute("serverURL", server);
+        model.addAttribute("serverURL", "placeholder.nl");
 
         return "fileInspectPage";
     }
