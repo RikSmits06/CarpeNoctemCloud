375d4be3c7d8fe319566523f623f8e519d6298d9
Thore <thorekoopmans@gmail.com>
2025-12-05 22:44:20 +0100

    Added new categories + icons

---
+++ src/main/resources/db/migration/V22__new_categories.sql
@@ -0,0 +1,6 @@
+insert into category(name, matching_extensions, svg_path)
+values ('Books', '{"epub", "azw3", "mobi", "azw", "cbr", "cbz", "pdb", "fb2", "ibooks", "lit", }', '/img/book.svg'),
+       ('PDF', '{"pdf"}', '/img/pdf.svg'),
+       ('Text Documents', '{"txt", "md", "docx", "otd", "rtf", "doc", "text"}', '/img/file-text.svg'),
+       ('Subtitles', '{"srt", "sub", "vtt", "ass"}', '/img/subtitle.svg'),
+       ('Archives', '{"zip", "rar", "7z", "tar", "gz", "bz2", "xz", }', '/img/archive.svg');
