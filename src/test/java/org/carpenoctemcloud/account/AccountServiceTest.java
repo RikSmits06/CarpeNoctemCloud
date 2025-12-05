15840f8bb62c058484a2663e611700b009987318
Rik Smits <h.j.smits@student.utwente.nl>
2025-12-05 16:21:51 +0100

    Made simple test for the account service.

--- src/test/java/org/carpenoctemcloud/account/AccountServiceTest.java
+++ src/test/java/org/carpenoctemcloud/account/AccountServiceTest.java
@@ -5,23 +5,33 @@ import org.carpenoctemcloud.CNCloudTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @CNCloudTest
 class AccountServiceTest {
 
-    private final String email = "h.j.smits@student.utwente.nl";
+
     @Autowired
     private AccountService accountService;
 
     @Test
     public void accountCreationTest() {
-        int id = accountService.createAccount("Rik Smits", email, "12345678", true);
+        String email = "test@student.utwente.nl";
+        String name = "Ben Dover";
+        int id = accountService.createAccount(name, email, "12345678", false);
         Optional<Account> accountOpt = accountService.getAccountByEmail(email);
         assertTrue(accountOpt.isPresent());
         Account account = accountOpt.get();
         assertEquals(id, account.id());
         assertEquals(email, account.email());
+        assertFalse(account.emailConfirmed());
+        assertFalse(account.isAdmin());
+        assertEquals(name, account.name());
+
+        accountService.activateAccount(id);
+        accountOpt = accountService.getActivatedAccountByEmail(email);
+        assertTrue(accountOpt.isPresent());
+        account = accountOpt.get();
+        assertTrue(account.emailConfirmed());
     }
 }
