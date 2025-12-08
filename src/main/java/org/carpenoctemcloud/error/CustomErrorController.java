ded6207aec52b25db0c6dafd9c78b7cc2e568110
rahimligabil <rahimliqabil0@gmail.com>
2025-12-08 17:46:12 +0300

    Change ErrorController more generic version

--- src/main/java/org/carpenoctemcloud/error/CustomErrorController.java
+++ src/main/java/org/carpenoctemcloud/error/CustomErrorController.java
@@ -12,29 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    @RequestMapping("/error")
+	@RequestMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
 
-        Object statusObj  = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        Object messageObj = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
-        Object pathObj    = request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+        Object path = request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
 
-        Integer statusCode = statusObj != null ? Integer.valueOf(statusObj.toString()) : 500;
+        model.addAttribute("status", status);
+        model.addAttribute("message", message);
+        model.addAttribute("path", path);
 
-        model.addAttribute("status", statusCode);
-        model.addAttribute("message", messageObj);
-        model.addAttribute("path", pathObj);
-
-        if (statusCode == HttpStatus.NOT_FOUND.value()) {
-            return "error/404";
-        }
-        if (statusCode == HttpStatus.FORBIDDEN.value()) {
-            return "error/403";
-        }
-        if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-            return "error/500";
-        }
-
-        return "error/500";
+        return "error"; // generic template
     }
 }
