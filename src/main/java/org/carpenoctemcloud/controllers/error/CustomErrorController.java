63b66d15f762d87141369c27274fe6e6175a0b90
rik <rikhjsmits@gmail.com>
2025-12-11 21:27:33 +0100

    Fixed the code

--- src/main/java/org/carpenoctemcloud/controllers/error/CustomErrorController.java
+++ src/main/java/org/carpenoctemcloud/controllers/error/CustomErrorController.java
@@ -1,28 +1,27 @@
-package org.carpenoctemcloud.error;
+package org.carpenoctemcloud.controllers.error;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/error")
 public class CustomErrorController implements ErrorController {
 
-	@RequestMapping("/error")
+    @GetMapping
     public String handleError(HttpServletRequest request, Model model) {
 
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
-        Object path = request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+        int status = (int) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String message = (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
 
         model.addAttribute("status", status);
         model.addAttribute("message", message);
-        model.addAttribute("path", path);
 
-        return "error"; // generic template
+        return "error";
     }
 }
